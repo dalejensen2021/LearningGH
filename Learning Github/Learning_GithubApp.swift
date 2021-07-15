f6473ef5d62efd5b571cab892db3c2970f729f2c
Dale Jensen <dale.jensen@aatrix.com>
2021-07-15 12:26:42 -0400

    Initial Commit

---
+++ Learning Github/Learning_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GithubApp.swift
+//  Learning Github
+//
+//  Created by Dale Jensen on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
